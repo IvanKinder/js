25760b29bc87182e5925f04ac3dbd4ec4c629db5
Ivan Kinder <ivankinder11@gmail.com>
2020-11-02 11:04:00 +0300

    hw3 четвертое задание

---
+++ 4.js
@@ -0,0 +1,30 @@
+"use strict";
+
+const products1 = [
+    {
+        id: 3,
+        price: 127,
+        photos: [
+            "1.jpg",
+            "2.jpg",
+        ]
+    },
+    {
+        id: 5,
+        price: 499,
+        photos: []
+    },
+    {
+        id: 10,
+        price: 26,
+        photos: [
+            "3.jpg"
+        ]
+    },
+    {
+        id: 8,
+        price: 78,
+    },
+];
+
+console.log(products1.filter(product => product.photos && product.photos.length > 0));
